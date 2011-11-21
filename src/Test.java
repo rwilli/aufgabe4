fc38830e842f74cadf7c9b234ebceff14bc9047f
Rainer Willinger <rainer.willinger@aon.at>
2011-11-21 22:53:30 +0100

    added test cases

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,16 @@
 import tree.Tree;
+import tree.integer.IntTree;
+import tree.integer.IntegerTree;
 import tree.string.InorderTree;
 import tree.string.PostorderTree;
 import tree.string.PreorderTree;
+import tree.string.ReplaceableTree;
 import tree.string.SortedTree;
 import tree.string.StringTree;
 
 /**
+ * Testclass
+ * 
  * @author Gruppe222
  *
  */
@@ -104,6 +109,85 @@ public class Test {
 		
 		// traverse method from PostorderTree class
 		System.out.println(((PreorderTree) t).traverse());
+		
+		/*
+		 * Testcase 4 ReplaceableTree
+		 */
+		t = new ReplaceableTree();
+		
+		((ReplaceableTree) t).add("D");
+		((ReplaceableTree) t).add("G");
+		((ReplaceableTree) t).add("B");
+		((ReplaceableTree) t).add("A");
+		((ReplaceableTree) t).add("C");
+		((ReplaceableTree) t).add("F");
+		((ReplaceableTree) t).add("I");
+		((ReplaceableTree) t).add("H");
+		((ReplaceableTree) t).add("J");
+		
+		// show tree method from abstract class Tree
+		System.out.println(t.toString());
+		
+		// contains method from abstract class StringTree
+		System.out.println(((StringTree) t).contains("I"));		// should work
+		System.out.println(((StringTree) t).contains("X")); 	// should fail
+		
+		// search method from Replaceable class
+		System.out.println(((SortedTree) t).search("H"));		// should work
+		System.out.println(((SortedTree) t).search("Z"));		// should fail
+		
+		// create temp tree for replace method
+		Tree t1 = new ReplaceableTree();
+		
+		((ReplaceableTree) t1).add("H");
+		((ReplaceableTree) t1).add("F");
+		((ReplaceableTree) t1).add("J");
+		((ReplaceableTree) t1).add("I");
+		((ReplaceableTree) t1).add("K");
+		
+		// replace method from Replaceable class
+		((ReplaceableTree) t).replace("F", t1.toString());
+		
+		// show tree method from abstract class Tree
+		System.out.println(t.toString());
+		
+		/*
+		 * Testcase IntTree
+		 */
+		t = new IntTree();
+		
+		((IntegerTree) t).add(4);
+		((IntegerTree) t).add(6);
+		((IntegerTree) t).add(8);
+		((IntegerTree) t).add(11);
+		((IntegerTree) t).add(2);
+		((IntegerTree) t).add(3);
+		
+		// show tree method from abstract class Tree
+		System.out.println(t.toString());
+		
+		// contains method from abstract class StringTree
+		System.out.println(((IntegerTree) t).contains(11));		// should work
+		System.out.println(((IntegerTree) t).contains(99)); 	// should fail
+		
+		// search method from Replaceable class
+		System.out.println(((IntegerTree) t).search(2));		// should work
+		System.out.println(((IntegerTree) t).search(67));		// should fail
+		
+		// create temp tree for replace method
+		t1 = new IntTree();
+		
+		((IntTree) t1).add(23);
+		((IntTree) t1).add(7);
+		((IntTree) t1).add(14);
+		((IntTree) t1).add(9);
+		((IntTree) t1).add(12);
+		
+		// replace method from Replaceable class
+		((IntTree) t).replace(2, t1.toString());
+		
+		// show tree method from abstract class Tree
+		System.out.println(t.toString());
 	}
 
 }
--- src/tree/integer/IntTree.java
+++ src/tree/integer/IntTree.java
@@ -9,7 +9,7 @@ public class IntTree extends IntegerTree {
 		
 	}
 	
-	public void replace(String position, String subTree) {
+	public void replace(int position, String subTree) {
 		// TODO implement method
 	}
 	
@@ -31,26 +31,14 @@ public class IntTree extends IntegerTree {
 	}
 	
 	private IntegerNode insert(IntegerNode node, int label) {
-		IntegerNode result = new IntegerNode(label);
 		
-		// leftside
-		if (label > 0) {
-		    if (result.getLeft() != null) {
-		    	result.setLeft(insert((IntegerNode) result.getLeft(), label));
-		    } else {
-		        result.setLeft(new IntegerNode(label));
-		        //size++;
-		    }
-		} else if (label < 0) {
-			// rightside
-		    if (result.getRight() != null) {
-		    	result.setRight(insert((IntegerNode) result.getRight(), label));
-		    } else {
-		        result.setRight(new IntegerNode(label));
-		        //size++;
-		    }
-		}
-		return result;
+		return node;
+	}
+
+	@Override
+	public boolean contains(int node) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
