005f224c04c4efdb338074e9a3f5387959fe9fd3
Rainer Willinger <rainer.willinger@aon.at>
2011-11-22 21:59:18 +0100

    added add and contain method

--- src/tree/string/ReplaceableTree.java
+++ src/tree/string/ReplaceableTree.java
@@ -1,8 +1,11 @@
 package tree.string;
 
+import stack.Stack;
 import node.INode;
+import node.Node;
 
 public class ReplaceableTree extends StringTree {
+	private String currentPath = "";
 
 	public ReplaceableTree() {
 		
@@ -19,20 +22,82 @@ public class ReplaceableTree extends StringTree {
 	
 	@Override
 	public boolean contains(String node) {
-		// TODO Auto-generated method stub
-		return false;
+		return search(node).equals("Knoten wurde nicht gefunden") ? false: true;
+
 	}
 
 	@Override
 	public String search(String node) {
-		// TODO Auto-generated method stub
-		return null;
+		this.currentPath = "";
+		
+		Stack s = new Stack();
+		
+		if (this.root != null) 
+			s.push(this.root);	
+		else 
+			return "Knoten wurde nicht gefunden";
+		  while ( !s.isEmpty() ) {                  
+		        INode n = s.pop();               
+		        do {
+		        	if(n.getLabel().equals(node)){
+		        		INode p = n.getParentNode();
+		        		while(p != null){
+		        			if(p.getLeftNode() == n)
+		        				this.currentPath =  "left "+this.currentPath;
+		        			else
+		        				this.currentPath =  "right "+this.currentPath;
+		        			n = p; 
+		        			p = p.getParentNode();
+		        		}
+		        		return this.currentPath;
+		        	}											
+		            if (n.getRightNode() != null){       
+		            	s.push(n.getRightNode());       
+		            }
+		            n = n.getLeftNode();               
+		        } while (n != null);           
+		    }
+		  return "Knoten wurde nicht gefunden";
 	}
 
 	@Override
 	public void add(String node) {
-		// TODO Auto-generated method stub
+		if (this.root == null) {
+	        this.root = new Node(node);
+	        this.root.setParentNode(null);
+	    } else {
+	         
+	    	INode n = findPlace(this.root, 0);
+	    	INode child = new Node(node);
+	    	child.setParentNode(n);
+	 
+	    	if(n.getLeftNode() == null){
+	    		n.setLeftNode(child);
+	    	}else{
+	    		n.setRightNode(child);
+	    	}
+	    }
+	}
+	
+	private INode findPlace(INode node, int depth){
 
+		node.setDepth(depth);
+		
+		if(node.getLeftNode() ==null || node.getRightNode() == null){
+			return node;
+		}else{
+			depth++;
+			INode n1,n2;
+			n1 = findPlace(node.getLeftNode(), depth);
+			n2 = findPlace(node.getRightNode(), depth);
+			
+			if(n1.getDepth() > n2.getDepth())
+				return n2;
+			else
+				return n1;
+					
+		}	
+		
 	}
 
 	public void replace(String position, String subTree) {
