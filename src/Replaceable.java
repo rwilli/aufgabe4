5e8d64247792e94043744abc70adadb69a927c47
patrickfuerst <patrickfuerst@106-166.swlan.wu-wien.ac.at>
2011-11-23 11:14:30 +0100

    edited

--- src/Replaceable.java
+++ src/Replaceable.java
@@ -7,8 +7,8 @@ public class Replaceable {
 		
 		if(root == null)
 			return;
-		
-		//TODO getNodes from subtree;
+		System.out.println("Position:  " + position);
+		System.out.println("Subtree:  " + subtree);
 		
 		
 		Scanner s = new Scanner(position);
@@ -16,24 +16,60 @@ public class Replaceable {
 		
 		while(s.hasNext()){
 			String temp = s.next();
-			if(temp == "left"){
+			System.out.println("Temp:  " + temp);
+
+			if(temp.contains("left")){
 				n = n.getLeftNode();
+				System.out.println("goLeftNode");
+
 			}else{
 				n = n.getRightNode();
-				
+				System.out.println("goRightNode");
+
 			}
 		}
-		
+		System.out.println("Node = " + n.getLabel());
+
 		//TODO Subtree an n hängen
-		
+		n = createTree(subtree);
 		
 	}
 	
-	private INode createTree(){
+	private INode createTree(String subtree){
 		
+		System.out.println("Subtree in Create:  " + subtree);
+
+		if(subtree.isEmpty())
+			return null;
 		
+		Scanner sc = new Scanner(subtree);
+		String s = sc.nextLine();
 		
+		int depth = s.length()-1;
 		
+		INode  n = new Node();
+		
+		n.setLabel(s.charAt(s.length()-1));
+		n.setDepthNode(depth);
+		
+		if(sc.hasNextLine()){
+			INode nLeft = createTree(sc.nextLine());
+			if(nLeft != null)
+				n.setLeftNode(nLeft);
+		}
+		while(sc.hasNextLine()){
+			
+			s = sc.nextLine();
+			if(s.length() == depth )
+				break;
+		}
+		if(sc.hasNextLine()){
+			INode nRight = createTree(s);
+			if(nRight != null)
+				n.setRightNode(nRight);
+		}
+	
+		return n;
 	}
 	
 
